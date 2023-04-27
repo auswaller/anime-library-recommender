7c2b885568a7193abe4d6266b597f303d6b88668
Austin W <auswaller@gmail.com>
2023-04-27 15:08:06 -0400

    added search by name functionality along with auto searching for name after finding image match

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,14 @@ let reviewsEl = document.getElementById("reviews");
 let addInfoEl = document.getElementById("additional-info");
 let episodesEl = document.getElementById("episodes");
 let searchImageButtonEl = document.getElementById("search-image-button");
-let searchNameButtonEl = document.getElementById("search-name-button")
+let searchNameButtonEl = document.getElementById("search-name-button");
+let searchNameTextEl = document.getElementById("search-name");
 let randomButtonEl = document.getElementById("random-button");
 let addToLibraryButtonEl = document.getElementById("add-library");
 
 let libraryItems = {id: "", title: ""};
 let imageSearchURL = "https://images.plurk.com/32B15UXxymfSMwKGTObY5e.jpg";
-
+let searchName = "cowboy bebop";
 
 init();
 
@@ -21,6 +22,14 @@ randomButtonEl.addEventListener("click", function(event){
     getRandomAnime();
 });
 
+searchNameButtonEl.addEventListener("click", function(event){
+    event.preventDefault();
+    console.log("Searched for name: " + searchNameTextEl.value);
+    getAnimeByName(searchNameTextEl.value);
+    searchNameTextEl.value = "";
+});
+
+/* These throw errors since the elements don't exist on page
 addToLibraryButtonEl.addEventListener("click", function(event){
     event.preventDefault();
     addToLibrary();
@@ -30,10 +39,12 @@ searchImageButtonEl.addEventListener("click", function(event){
     event.preventDefault();
     getAnimeByImage();
 });
-
+*/
 
 function init(){
     libraryItems = loadFromLocalStorage("library");
+
+    //For testing
     getAnimeByImage();
 }
 
@@ -47,19 +58,20 @@ function getRandomAnime(){
             return response.json();
         }
 
-        throw new Error("Something went wrong. Try the search again");
+        throw new Error("Something went wrong with finding a random anime. Trying the search again");
     }).then(function(data){
         console.log("----Random Anime Data----");
         console.log(data);
 
-        buildShowDisplay(data);
+        buildShowDisplay(data, "random");
     }).catch(function(error){
         console.log(error);
+        getRandomAnime();
     });
 }
 
 function getAnimeByImage(){
-    fetch(`https://api.trace.moe/search?url=${encodeURIComponent(imageSearchURL)}`
+    fetch(`https://api.trace.moe/search?anilistInfo&url=${encodeURIComponent(imageSearchURL)}`
     ).then(function(response){
         console.log("----Image Anime Response----");
         console.log(response);
@@ -68,26 +80,69 @@ function getAnimeByImage(){
             return response.json();
         }
 
-        throw new Error("Something went wrong");
+        throw new Error("Something went wrong with searching by image. Try the search again");
     }).then(function(data){
         console.log("----Image Anime Data----");
         console.log(data);
+
+        getAnimeByName(data.result[0].anilist.title.romanji);
     }).catch(function(error){
         console.log(error);
     });    
 }
 
-function buildShowDisplay(showRawInfo){
-    let showId = showRawInfo.data.id;
-    let showTitle = showRawInfo.data.attributes.canonicalTitle;
-    let showSynopsis = showRawInfo.data.attributes.description;
-    let showPoster = showRawInfo.data.attributes.posterImage.large;
-    let showReviews = showRawInfo.data.attributes.averageRating;
-    let showEpisodes = showRawInfo.data.attributes.episodeCount;
-    let showYoutubeID = showRawInfo.data.attributes.youtubeVideoId; //Add "https://www.youtube.com/watch?v=" before this to get the full url
+function getAnimeByName(searchName){
+    fetch("https://kitsu.io/api/edge/anime?filter[text]=" + searchName)
+    .then(function(response){
+        console.log("----Search Anime Response----");
+        console.log(response);
+
+        if(response.status === 200){
+            return response.json();
+        }
+
+        throw new Error("Something went wrong with finding an anime. Try the search again");
+    }).then(function(data){
+        console.log("----Search Anime Data----");
+        console.log(data);
+
+        buildShowDisplay(data.data[0], "name");
+    }).catch(function(error){
+        console.log(error);
+    });
+}
+
+function buildShowDisplay(showRawInfo, whereFrom){
+    console.log(showRawInfo);
+    let showId;
+    let showTitle;
+    let showSynopsis;
+    let showPoster;
+    let showReviews;
+    let showEpisodes;
+    let showYoutubeID;
+
+    if(whereFrom === "random"){
+        showId = showRawInfo.data.id;
+        showTitle = showRawInfo.data.attributes.canonicalTitle;
+        showSynopsis = showRawInfo.data.attributes.description;
+        showPoster = showRawInfo.data.attributes.posterImage.large;
+        showReviews = showRawInfo.data.attributes.averageRating;
+        showEpisodes = showRawInfo.data.attributes.episodeCount;
+        showYoutubeID = showRawInfo.data.attributes.youtubeVideoId; //Add "https://www.youtube.com/watch?v=" before this to get the full url
+    }
+    else{
+        showId = showRawInfo.id;
+        showTitle = showRawInfo.attributes.canonicalTitle;
+        showSynopsis = showRawInfo.attributes.description;
+        showPoster = showRawInfo.attributes.posterImage.large;
+        showReviews = showRawInfo.attributes.averageRating;
+        showEpisodes = showRawInfo.attributes.episodeCount;
+        showYoutubeID = showRawInfo.attributes.youtubeVideoId; //Add "https://www.youtube.com/watch?v=" before this to get the full url
+    }
 
     console.log(showTitle + " | " + showId + " | " + showSynopsis + " | " + showPoster + " | " + showReviews + " | " + showEpisodes + " | " + showYoutubeID);
-    
+
     //Build out show display page here. Make sure to add "data-" elements with the info for each element in order to save them to library. At least the show title and ID
 }
 
